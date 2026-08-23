394ed407871e6af7c949d9b73a87d4a86cb3293e
Gaurav Chaudhary <gauravjaat456780@gmail.com>
2026-08-23 12:16:57 +0530

    Add: implement-stack-using-queues - submission-2

---
+++ Data Structures & Algorithms/implement-stack-using-queues/submission-2.java
@@ -0,0 +1,39 @@
+class MyStack {
+    Queue<Integer> q = new LinkedList<>();
+
+    public MyStack() {
+    }
+
+    public void push(int x) {
+        q.add(x);
+    }
+
+    public int pop() {
+        int n = q.size();
+
+        for (int i = 1; i <= n - 1; i++) {
+            q.add(q.remove());
+        }
+
+        return q.remove();
+    }
+
+    public int top() {
+        int n = q.size();
+
+        for (int i = 1; i <= n - 1; i++) {
+            q.add(q.remove());
+        }
+
+        int p = q.peek();
+
+        // Restore original order
+        q.add(q.remove());
+
+        return p;
+    }
+
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
